1ad9e07c353037ec767ba1fc1cd4638baa47e877
Daniel <danildeyneka@gmail.com>
2026-01-26 13:44:13 +0500

    fix: session error

--- src/checkAuth.ts
+++ src/checkAuth.ts
@@ -4,16 +4,11 @@ export const checkAuth = async (page: Page, browser: Browser) => {
 	await page.goto('https://hh.ru/applicant/resumes', {
 		waitUntil: 'domcontentloaded',
 		timeout: 10000,
-	});
-	
-	const login = page.locator('[data-qa="applicantProfilePage-button"]');
-	const isLogged = await login.count()
-	
-	if (isLogged) {
-		console.log('Вы успешно авторизованы!');
-	} else {
+	}).catch(async () => {
 		console.log('Ошибка авторизации на hh.ru! Запустите скрипт renew-cookie');
 		await browser.close();
 		process.exit(1);
-	}
+	});
+	
+	console.log('Вы успешно авторизованы!');
 }
