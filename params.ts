8d04443c1b3779fe53521cab588a3b2e1a18af14
Danila Deyneka <serzhhhors@yandex.ru>
2026-03-22 23:23:22 +0500

    chore: cover letter

--- params.ts
+++ params.ts
@@ -2,7 +2,7 @@
 export const VACANCIES_LIST = 'https://hh.ru/search/vacancy?text=Frontend&search_field=name&order_by=relevance';
 export const VACANCIES_LIST_ZARPLATA = 'https://zarplata.ru/search/vacancy?text=Frontend&search_field=name&order_by=relevance';
 export const COVER_LETTER = 'Здравствуйте, я - Данила Сергеевич, Frontend разработчик с коммерческим опытом 4,5 года (больше 2х из которых - финтех).\n' +
-	'Работал со всем стеком вакансии, Typescript + React - основной. Работаю со вебом и SEO, умею делать telegram ботов на NodeJS.\n' +
+	'Работал со всем стеком вакансии, Typescript + React - основной. Работаю со вебом, SEO, CI/CD.\n' +
 	'Рассматриваю исключительно удаленный формат работы.\n' +
 	'Оперативно отвечу в telegram - @frontendick'
 // скрывать вакансию после обработки
